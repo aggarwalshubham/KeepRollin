7c1f96ede8c9b1499174d7c8fcc67287d049dbbe
Shubham Aggarwal <58016905+aggarwalshubham@users.noreply.github.com>
2020-04-26 22:36:12 -0400

    Updated server side code - Minor modifications
    Comments updated, indentation updated and file descriptors properly closed.

--- server.c
+++ server.c
@@ -1,5 +1,5 @@
 //Advanced Systems Programming Final Project Submission
-//By Shubham Aggarwal(110008557) and Deepti Bamel (110008446)
+//By Shubham Aggarwal(110008557)
 //Server-Side Implementation of 2-player DICE GAME using Socket Programming
 
 #include <unistd.h>
@@ -10,22 +10,23 @@
 #include <string.h>
 #include <stdlib.h>
 #define PORT 8976
+//Port has been hard coded. It can be taken from the command line as well.
 
 void servicePlayers(int, int);
 
 int main(int argc, char const *argv[])
 {
-    int sock_descriptor =0;
+    	int sock_descriptor =0;
     
 	struct sockaddr_in address;
 	//Communication Domain - AF_INET(IPv4 Protocol)
 	address.sin_family = AF_INET;
 	//Using local host address
-    address.sin_addr.s_addr = INADDR_ANY;
+   	address.sin_addr.s_addr = INADDR_ANY;
 	//Assigning port
-    address.sin_port = htons(PORT);
+    	address.sin_port = htons(PORT);
 		
-	//Client socket descriptors
+	//Client side file descriptors
 	int client_socket_1;
 	int client_socket_2;
 	
@@ -36,30 +37,30 @@ int main(int argc, char const *argv[])
 	char *error_message_4 = "Error encountered while socket creation for PLAYER 1...";
 	char *error_message_5 = "Error encountered while socket creation for PLAYER 2...";
 	
-    //Socket created using:
+    	//Socket created using:
 	//Communication Domain - AF_INET : IPv4 Protocol
 	//Communication Type - SOCK_STREAM : TCP(reliable, connection oriented)
 	//Protocol - 0 : Internet Protocol
-    if ((sock_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {  
+    	if ((sock_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    	{  
 		perror(error_message_1);
-        exit(EXIT_FAILURE);
-    }
+        	exit(EXIT_FAILURE);
+    	}
     
-    //Bind newly created socket to given IP address and verify
-    if (bind(sock_descriptor, (struct sockaddr *)&address, sizeof(address))<0)
-    {   
+    	//Bind newly created socket to given IP address and verify
+    	if (bind(sock_descriptor, (struct sockaddr *)&address, sizeof(address))<0)
+    	{   
 		perror(error_message_2);
-        exit(EXIT_FAILURE);
-    }
+        	exit(EXIT_FAILURE);
+    	}
     
 	//Putting server socket in PASSIVE mode, where it can wait for clients to approach for connection initialisation
 	//Backlog queue is initialised to 3 connections
 	if (listen(sock_descriptor, 3) != 0)
-    {   
+    	{   
 		perror(error_message_3);
-        exit(EXIT_FAILURE);
-    }
+        	exit(EXIT_FAILURE);
+    	}
     
 	printf("---------------TWO PLAYER DICE GAME---------------\n");
 	printf("Waiting for players to join...\n\n");
@@ -67,10 +68,9 @@ int main(int argc, char const *argv[])
 	while(1){
 		
 		//Extracts first connection request from pending connection queue and establishes connection
-		if ((client_socket_1 = accept(sock_descriptor, (struct sockaddr *)&address, (socklen_t*)&address))<0)
-    	{
+		if ((client_socket_1 = accept(sock_descriptor, (struct sockaddr *)&address, (socklen_t*)&address))<0){
 			perror(error_message_4);
-	       	exit(EXIT_FAILURE);
+	       		exit(EXIT_FAILURE);
 		}
 		printf("Player 1 - TOTO has joined, Waiting for another player to join...\n");
     	
@@ -87,7 +87,7 @@ int main(int argc, char const *argv[])
 		}
 		close(client_socket_1);
 		close(client_socket_2);
-    }
+    	}
     return 0;
 }
 
@@ -98,14 +98,14 @@ void servicePlayers(int player1, int player2){
 	
 	char *instruction_msg = "You can now play";
 	char *winning_msg= "Game over: you won the game";
-    char *loosing_msg= "Game over: you lost the game";
+    	char *loosing_msg= "Game over: you lost the game";
 	
 	//int win_msg_length = strlen(winning_msg);
 	//int loss_msg_length = strlen(loosing_msg);
 	
 	while(1){    
 		char buffer1_score[1024]={0};
-	    char buffer2_score[1024]={0};
+	    	char buffer2_score[1024]={0};
 		
 		sleep(1);
 		
@@ -122,7 +122,8 @@ void servicePlayers(int player1, int player2){
 		//Condition - if Player 1 - TOTO's Total Score has reached 100
 		if(player1_total_score >= 100){
 			write(player1, winning_msg, strlen(winning_msg));
-           	write(player2, loosing_msg, strlen(loosing_msg));
+           		write(player2, loosing_msg, strlen(loosing_msg));
+			close(player1);
 			exit(0);
 		}
 		
@@ -136,21 +137,19 @@ void servicePlayers(int player1, int player2){
 			exit(1);
 		}
 
-        player2_total_score = player2_total_score + atoi(buffer2_score);
+        	player2_total_score = player2_total_score + atoi(buffer2_score);
 		printf("Player 2 - TITI's Total Score:%d \n\n", player2_total_score); 
 		
 		
 		//Condition - if Player 2 - TITI's Total Score has reached 100
 		if(player2_total_score >= 100){
-            write(player2, winning_msg, strlen(winning_msg));
-           	write(player1, loosing_msg, strlen(loosing_msg));
+           		write(player2, winning_msg, strlen(winning_msg));
+           		write(player1, loosing_msg, strlen(loosing_msg));
+			close(player2);
 			exit(0);
-        }
+        	}
 		
 		printf("\n");
 		
 	}
-	
-	close(player1);
-	close(player2);
 }
